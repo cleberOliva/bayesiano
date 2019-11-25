38eefd4e94e2f02750e858adac08c98e55c36413
cleberOliva <cleoliva.pincerato@gmail.com>
2019-11-25 00:06:42 -0300

    Quase lá!

--- classifierBayesiano.py
+++ classifierBayesiano.py
@@ -2,49 +2,57 @@
 import sys
 class ClassificadorBayesiano:
     def main(self):
-        _dataset = []
-        _questions = []
+        x_dataset = []
+        x_questions = []
         # get file name by the arguments
-        _filename = sys.argv[1]
+        x_filename = sys.argv[1]
         
         # open file
-        _file = open(_filename, "r")
-        _cabecalho = _file.readline().replace('\n', '').split(' ')
+        x_file = open(x_filename, "r")
+        x_cabecalho = x_file.readline().replace('\n', '').split(' ')
         # print(_cabecalho[-1])
         # print(_cabecalho)
         
-        for lines in _file:
-            _replace = lines.replace('\n', '')
-            if _replace != '---':
-                _dataset.append(_replace.split(' '))
+        for lines in x_file:
+            x_replace = lines.replace('\n', '')
+            if x_replace != '---':
+                x_dataset.append(x_replace.split(' '))
             else:
                 break
-        for lines in _file:
-            _replace = lines.replace('\n', '')
-            _questions.append(_replace.split(' '))
-        _file.close()
+        for lines in x_file:
+            x_replace = lines.replace('\n', '')
+            x_questions.append(x_replace.split(' '))
+        x_file.close()
 
-        _dictionary = {}
-        for i in range(len(_dataset)):
-            _linha = _dataset[i]
-            if (_linha[-1] not in _dictionary):
-                _dictionary[_linha[-1]] = []
-            _dictionary[_linha[-1]].append(_linha)
+        x_dictionary = {}
+        for i in range(len(x_dataset)):
+            x_linha = x_dataset[i]
+            if (x_linha[-1] not in x_dictionary):
+                x_dictionary[x_linha[-1]] = []
+            x_dictionary[x_linha[-1]].append(x_linha)
         
-        print((_dictionary['N'][0][0]))
-        print(len(_dictionary['S']))
-        for _index in _dictionary:
-            print(_index)
-            for _counter_line_questions in range(len(_questions)):
-                for _counter_column in range(len(_cabecalho)):
-                    print('Elemento Questão:    ' + str(_questions[_counter_line_questions][_counter_column]))
-                    _somatorio = 0
-                    for _counter_line_dict in range(len(_dictionary[_index])):
-                        # print('Elemento Dicionario: ' + str(_dictionary[_index][_counter_line_dict][_counter_column]))
-                        if (_questions[_counter_line_questions][_counter_column] == _dictionary[_index][_counter_line_dict][_counter_column]):
-                            _somatorio = _somatorio + 1
-                    # _porcentagem = _somatorio / len(_dictionary[_index])
-                    # print(_porcentagem)
-                    # print('=========================================')
-                    print(_cabecalho[_counter_column] + '=' + str(_questions[_counter_line_questions][_counter_column]))
+        print((x_dictionary['N'][0][0]))
+        print(len(x_dictionary['S']))
+
+        for x_counter_line_questions in range(len(x_questions)):
+            for x_index in x_dictionary:
+                print(x_index)
+                x_produtorio = 1
+                for x_counter_column in range(len(x_cabecalho)):
+                    print('Elemento Questão:    ' + str(x_questions[x_counter_line_questions][x_counter_column]))
+                    if x_questions[x_counter_line_questions][x_counter_column] == '?':
+                        pass
+                    else:
+                        x_somatorio = 0
+                        for x_counter_line_dict in range(len(x_dictionary[x_index])):
+                            print('Elemento Dicionario: ' + str(x_dictionary[x_index][x_counter_line_dict][x_counter_column]))
+                            if (x_questions[x_counter_line_questions][x_counter_column] == x_dictionary[x_index][x_counter_line_dict][x_counter_column]):
+                                x_somatorio = x_somatorio + 1
+                        x_porcentagem = x_somatorio / len(x_dictionary[x_index])
+                        x_produtorio = x_produtorio * x_porcentagem
+                        print('%.4f' % x_porcentagem)
+                
+                print('%.4f' % x_produtorio)
+                print('=========================================')
+                # print(_cabecalho[_counter_column] + '=' + str(_questions[_counter_line_questions][_counter_column]))
 ClassificadorBayesiano().main()
