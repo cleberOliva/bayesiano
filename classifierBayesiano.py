7324888858eb0a5ca80952412f615515c5f3f3a0
cleberOliva <cleoliva.pincerato@gmail.com>
2019-11-24 23:35:28 -0300

    Quase lá!

--- classifierBayesiano.py
+++ classifierBayesiano.py
@@ -1,6 +1,6 @@
 # https://gist.github.com/tuttelikz/94f750ef3bf14f8a126a
 import sys
-class Main:
+class ClassificadorBayesiano:
     def main(self):
         _dataset = []
         _questions = []
@@ -31,15 +31,20 @@ class Main:
                 _dictionary[_linha[-1]] = []
             _dictionary[_linha[-1]].append(_linha)
         
-        print("Cabecalho")
-        print(_cabecalho)
-        print
+        print((_dictionary['N'][0][0]))
+        print(len(_dictionary['S']))
         for _index in _dictionary:
             print(_index)
-            for _atributos in _dictionary[_index]:
-                print(_atributos)
-            print
-        print("Perguntas")
-        for _linha in _questions:
-            print(_linha)
-Main().main()
+            for _counter_line_questions in range(len(_questions)):
+                for _counter_column in range(len(_cabecalho)):
+                    print('Elemento Questão:    ' + str(_questions[_counter_line_questions][_counter_column]))
+                    _somatorio = 0
+                    for _counter_line_dict in range(len(_dictionary[_index])):
+                        # print('Elemento Dicionario: ' + str(_dictionary[_index][_counter_line_dict][_counter_column]))
+                        if (_questions[_counter_line_questions][_counter_column] == _dictionary[_index][_counter_line_dict][_counter_column]):
+                            _somatorio = _somatorio + 1
+                    # _porcentagem = _somatorio / len(_dictionary[_index])
+                    # print(_porcentagem)
+                    # print('=========================================')
+                    print(_cabecalho[_counter_column] + '=' + str(_questions[_counter_line_questions][_counter_column]))
+ClassificadorBayesiano().main()
